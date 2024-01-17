b69e78fd29ec6296523fa74af7f373cb53e2cb51
Ömer Faruk BARAN <omerfaruk38baran@gmail.com>
2024-01-17 20:00:32 +0300

    Update SQL ÖDEV -11.sql

--- SQL DERSLERİ - 2/SQL ÖDEV -11.sql
+++ SQL DERSLERİ - 2/SQL ÖDEV -11.sql
@@ -34,7 +34,12 @@ FROM customer;
 */
 
 -- 4. İlk 3 sorguyu tekrar eden veriler için de yapalım.
+/*
+SELECT first_name FROM actor;
+UNION ALL
+SELECT first_name FROM customer;
 
-
-BOŞ
-#########################################################
+SELECT first_name FROM actor
+EXCEPT ALL
+SELECT first_name FROM customer;
+*/
